712394f6deddf9ef214cd403bf9124380c11ba3a
Sooraj Pottekat <Sooraj.Pottekat@multichoice.co.za>
2017-10-21 13:18:00 +0530

    Create Cell class and the tests associated with it

---
+++ src/main/java/com/thoughtworks/game/component/component/Cell.java
@@ -0,0 +1,31 @@
+package com.thoughtworks.game.component.component;
+
+/**
+ * Created by Sooraj.Pottekat on 10/16/2017.
+ *
+ * @author Sooraj Pottekat
+ */
+public class Cell
+{
+
+    private boolean isAlive;
+
+    public Cell()
+    {
+        isAlive = true;
+    }
+    public boolean isAlive()
+    {
+        return isAlive;
+    }
+
+    public void dies()
+    {
+        isAlive = false;
+    }
+
+    public void reproduce()
+    {
+        isAlive = true;
+    }
+}
---
+++ src/test/java/com/thoughtworks/game/component/CellTest.java
@@ -0,0 +1,54 @@
+package com.thoughtworks.game.component;
+
+import com.thoughtworks.game.component.component.Cell;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/*
+ * Copyright (c) Multichoice Technical Operations. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Multichoice Technical Operations. ("Confidential Information"). You
+ * shall not disclose such Confidential Information and shall use it
+ * only in accordance with the terms of the license agreement you
+ * entered into with Multichoice Technical Operations.
+ *
+ * MULTICHOICE MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
+ * SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. MULTICHOICE
+ * SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT
+ * OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ */
+
+/**
+ * Created by Sooraj.Pottekat on 10/16/2017.
+ *
+ * @author Sooraj Pottekat
+ */
+public class CellTest
+{
+    @Test
+    public void createCell_stateAlive()
+    {
+        assertTrue(new Cell().isAlive());
+    }
+    @Test
+    public void createdCell_dies()
+    {
+        final Cell cell = new Cell();
+        cell.dies();
+        assertFalse(cell.isAlive());
+    }
+
+    @Test
+    public void deadCell_reproduces()
+    {
+        final Cell cell = new Cell();
+        cell.dies();
+        assertFalse(cell.isAlive());
+        cell.reproduce();
+        assertTrue(cell.isAlive());
+    }
+}
