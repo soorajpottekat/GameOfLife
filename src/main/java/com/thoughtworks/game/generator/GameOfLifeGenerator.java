aae567397813e2636641ff0650fb95051a1266e8
Sooraj Pottekat <Sooraj.Pottekat@multichoice.co.za>
2017-11-02 12:33:35 +0530

    Remove the unwanted header from GameOfLifeGenerator

--- src/main/java/com/thoughtworks/game/generator/GameOfLifeGenerator.java
+++ src/main/java/com/thoughtworks/game/generator/GameOfLifeGenerator.java
@@ -1,20 +1,4 @@
 package com.thoughtworks.game.generator;
-/*
- * Copyright (c) Multichoice Technical Operations. All Rights Reserved.
- *
- * This software is the confidential and proprietary information of
- * Multichoice Technical Operations. ("Confidential Information"). You
- * shall not disclose such Confidential Information and shall use it
- * only in accordance with the terms of the license agreement you
- * entered into with Multichoice Technical Operations.
- *
- * MULTICHOICE MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
- * SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING
- * BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. MULTICHOICE
- * SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT
- * OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
- */
 
 import com.thoughtworks.game.map.Map;
 import com.thoughtworks.game.rule.RuleSet;
